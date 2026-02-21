2368e6324e57f55dbc337677885198c3f36eb639
patilyogesh22 <patilyogesh222004@gmail.com>
2026-02-21 21:04:46 +0530

    Complete IaC provisioning, Dockerized Flask app, and Kubernetes setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "telecomm_ec2" {
   ami           = "ami-0f5ee92e2d63afc18" # Ubuntu 22.04 (check region)
-  instance_type = "t2.small"
+  instance_type = "t3.micro"
   key_name      = "medicapskey1"
 
   user_data = <<-EOF
